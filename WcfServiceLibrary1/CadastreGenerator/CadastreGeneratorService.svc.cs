fddf6f3b434467efe810d78467c693704c265ee1
angerlina <iratnovskaya@gmail.com>
2016-11-19 19:23:34 +0400

    CadastreGenerator

---
+++ WcfServiceLibrary1/CadastreGenerator/CadastreGeneratorService.svc.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace CadastreGenerator
+{
+    public class CadastreGeneratorService : ICadastreGeneratorService
+    {
+        Random rnd = new Random();
+
+        private string GenerateRegionNumber()
+        {
+            return rnd
+                .Next(1, 99)
+                .ToString()
+                .PadLeft(2, '0');
+        }
+
+        private string GenerateAdministrativeDistrict()
+        {
+            return rnd
+                .Next(1, 99)
+                .ToString()
+                .PadLeft(2, '0');
+        }
+
+        private string GenerateCadastreQuarter()
+        {
+            return rnd
+                .Next(999999, 9999999)
+                .ToString();
+        }
+
+        private string GenerateCadastreArea()
+        {
+            return rnd
+                .Next(01, 99)
+                .ToString()
+                .PadLeft(2, '0');
+        }
+
+        public string Generate()
+        {
+            return GenerateRegionNumber()
+                   + ":"
+                   + GenerateAdministrativeDistrict()
+                   + ":"
+                   + GenerateCadastreQuarter()
+                   + ":"
+                   + GenerateCadastreArea();
+        }
+    }
+}
