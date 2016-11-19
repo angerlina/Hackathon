7b597dfb8b7d662d68c02056665652873b8937d6
angerlina <iratnovskaya@gmail.com>
2016-11-19 13:59:20 +0400

    1

---
+++ WcfServiceLibrary1/WcfService6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.IO;
+using System.Net;
+
+
+namespace WcfService6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseAddress = "http://" + "localhost:8000/";
+            ServiceHost host = new ServiceHost(typeof(Service1), new Uri(baseAddress));
+
+            host.AddServiceEndpoint(typeof(IService1), new WebHttpBinding(), "").Behaviors.Add(new WebHttpBehavior());
+            host.Open();
+            //HttpWebRequest req = (HttpWebRequest)HttpWebRequest.Create(baseAddress + "/Generate/ss");
+            //req.Method = "GET";
+
+
+        }
+    }
+}
---
+++ WcfServiceLibrary1/WcfService6/RankGenerator.svc.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RankGenerator
+{
+    public class RankGenerator
+    {
+        private List<string> rankList = new List<string>();
+
+        public RankGenerator()
+        {
+            rankList.Add("Рядовой (курсант)");
+            rankList.Add("Ефрейтор");
+            rankList.Add("Младший сержант");
+            rankList.Add("Сержант");
+            rankList.Add("Старший сержант");
+            rankList.Add("Старшина");
+            rankList.Add("Прапорщик");
+            rankList.Add("Старший прапорщик");
+            rankList.Add("Младший лейтенант");
+            rankList.Add("Лейтенант");
+            rankList.Add("Старший лейтенант");
+            rankList.Add("Капитан");
+            rankList.Add("Майор");
+            rankList.Add("Подполковник");
+            rankList.Add("Полковник");
+            rankList.Add("Генерал-майор");
+            rankList.Add("Генерал-лейтенант");
+            rankList.Add("Генерал-полковник");
+            rankList.Add("Генерал армии");
+            rankList.Add("Маршал Российской Федерации");
+            rankList.Add("Матрос (курсант)");
+            rankList.Add("Старший матрос");
+            rankList.Add("Старшина 2 статьи");
+            rankList.Add("Старшина 1 статьи");
+            rankList.Add("Главный старшина");
+            rankList.Add("Главный корабельный старшина");
+            rankList.Add("Мичман");
+            rankList.Add("Старший мичман");
+            rankList.Add("Младший лейтенант");
+            rankList.Add("Лейтенант");
+            rankList.Add("Старший лейтенант");
+            rankList.Add("Капитан-лейтенант");
+            rankList.Add("Капитан 3 ранга");
+            rankList.Add("Капитан 2 ранга");
+            rankList.Add("Капитан 1 ранга");
+            rankList.Add("Контр-адмирал");
+            rankList.Add("Вице-адмирал");
+            rankList.Add("Адмирал");
+            rankList.Add("Адмирал флота");
+        }
+
+        public string ReturnRandomRank()
+        {
+            Random rnd = new Random();
+            int listIndex = rnd.Next(rankList.Capacity - 1);
+            return rankList[listIndex];
+        }
+    }
+}
