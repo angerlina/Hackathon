091d4857ca7bc7bd2ec209b21d8d72f2c966fbe5
overl0rd <gmail@gmail.com>
2016-11-19 22:05:56 +0400

    add test classes

---
+++ WcfServiceLibrary1/CvvGenerator/CvvGeneratorServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace CvvGenerator
+{
+    [TestFixture]
+    public class CvvGeneratorServiceTests
+    {
+        [Test]
+        public void GeneratorReturnsStringForIndividual()
+        {
+            CvvGeneratorService testExample = new CvvGeneratorService();
+            var result = testExample.Generate();
+            Assert.IsTrue(result is string);
+        }
+
+        [Test]
+        public void CheckReturningLength()
+        {
+            CvvGeneratorService testExample = new CvvGeneratorService();
+            string result = testExample.Generate();
+            Assert.IsTrue(result.Length == 3);
+        }
+    }
+}
---
+++ WcfServiceLibrary1/EmailGenerator/EmailGeneratorServiceTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace EmailGenerator
+{
+    [TestFixture]
+    public class EmailGeneratorServiceTests
+    {
+        [Test]
+        public void GeneratorReturnsStringForIndividual()
+        {
+            EmailGeneratorService testExample = new EmailGeneratorService();
+            var result = testExample.Generate("yandex.ru");
+            Assert.IsTrue(result is string);
+        }
+
+        [Test]
+        public void GeneratorReturnsMathingResult()
+        {
+            string domain = "yandex.ru";
+            EmailGeneratorService testExample = new EmailGeneratorService();
+            var result = testExample.Generate(domain);
+            StringAssert.IsMatch(@"^.{1,}\@.{1,}\..{1,}", result);
+        }
+
+        [Test]
+        public void ResultEndWithParameter()
+        {
+            string domain = "yandex.ru";
+            EmailGeneratorService testExample = new EmailGeneratorService();
+            var result = testExample.Generate(domain);
+            StringAssert.EndsWith(domain, result);
+        }
+    }
+}
