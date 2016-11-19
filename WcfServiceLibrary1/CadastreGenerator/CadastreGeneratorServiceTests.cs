8f3b363de7aaa9a66925ea50a283d7dd630d2e72
overl0rd <gmail@gmail.com>
2016-11-19 19:41:42 +0400

    added test class for cadastre generator

---
+++ WcfServiceLibrary1/CadastreGenerator/CadastreGeneratorServiceTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace CadastreGenerator
+{
+    [TestFixture]
+    public class CadastreGeneratorServiceTests
+    {
+        [Test]
+
+
+    }
+}
