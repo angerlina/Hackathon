798ae5ab611915734f241660ad0ba7bac4d61641
overl0rd <gmail@gmail.com>
2016-11-19 19:43:17 +0400

    rejected

---
+++ WcfServiceLibrary1/KppGenerator/KppGeneratorServiceTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace KppGenerator
+{
+    [TestFixture]
+    public class KppGeneratorServiceTests
+    {
+        [Test]
+        public void GenerateTestReturnsString()
+        {
+            KppGeneratorService testExemplair = new KppGeneratorService();
+            var result = testExemplair.Generate();
+            Assert.IsTrue(result is string);
+
+        }
+
+        [Test]
+        public void GenerateTestReturnsEnoughSymbols()
+        {
+            KppGeneratorService testExemplair = new KppGeneratorService();
+            var result = testExemplair.Generate();
+            Assert.IsTrue(result.Length == 9);
+        }
+    }
+}
