2ef56f0c837d5a29ba843eb88ec84c489e44a1b7
angerlina <iratnovskaya@gmail.com>
2016-11-19 21:07:18 +0400

    CVV Generator

---
+++ WcfServiceLibrary1/CvvGenerator/CvvGeneratorService.svc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace CvvGenerator
+{  
+    public class CvvGeneratorService : ICvvGeneratorService
+    {
+        Random r = new Random();
+        public string Generate()
+        {
+            return r.Next(0,999).ToString("D3");
+        }
+    }
+}
---
+++ WcfServiceLibrary1/CvvGenerator/ICvvGeneratorService.cs
@@ -0,0 +1,15 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace CvvGenerator
+{
+    [ServiceContract]
+    public interface ICvvGeneratorService
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "/Generate",
+            RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped)]
+        string Generate();
+    }
+}
