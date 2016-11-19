b59f6bcab1e6949fe312ae588cc2a23a8dec1cbe
angerlina <iratnovskaya@gmail.com>
2016-11-19 23:31:39 +0400

    WhomPassportIssuedGenerator

---
+++ WcfServiceLibrary1/WhomPassportIssuedGenerator/IWhomPassportIssuedGeneratorService.cs
@@ -0,0 +1,15 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace WhomPassportIssuedGenerator
+{
+    [ServiceContract]
+    public interface IWhomPassportIssuedGeneratorService
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "/Generate",
+            RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped)]
+        string Generate();
+    }
+}
---
+++ WcfServiceLibrary1/WhomPassportIssuedGenerator/WhomPassportIssuedGeneratorService.svc.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WhomPassportIssuedGenerator
+{
+    public class WhomPassportIssuedGeneratorService : IWhomPassportIssuedGeneratorService
+    {
+        private string[] departments =
+        {
+            "Отделом Внутренних Дел",
+            "Управлением Внутренних Дел",
+            "Районным Отделением Внутренних Дел",
+            "Паспортным столом",
+            "Федеральной Миграционной службой"
+        };
+
+        private string[] blocks =
+        {
+            "Промышленного района",
+            "Кировского района",
+            "Советского района",
+            "Октябрьского района",
+            "Железнодорожного района",
+            "Красноглинского района",
+            "Куйбышевского района",
+            "Ленинского района",
+            "Самарского района"
+        };
+
+        private string[] cities =
+        {
+            "города Самары",
+            "города Тольятти",
+            "города Ульяновска"
+        };
+
+        Random rndm = new Random();
+
+        private int GetRandomStringArrayIndex(string[] array)
+        {
+            return rndm.Next(array.Length);
+        }
+
+        public string Generate()
+        {
+            string department = departments[GetRandomStringArrayIndex(departments)];
+            string block = blocks[GetRandomStringArrayIndex(blocks)];
+            string city = cities[GetRandomStringArrayIndex(cities)];
+            return "Паспорт выдан:" + " " + department + ' ' + block + ' ' + city;
+        }
+    }
+}
