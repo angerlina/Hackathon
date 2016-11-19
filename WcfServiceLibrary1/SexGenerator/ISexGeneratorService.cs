7d051fcae5f2c01a4b9ab7aabe04077d220d42a7
angerlina <iratnovskaya@gmail.com>
2016-11-19 23:10:40 +0400

    Sex Generator

---
+++ WcfServiceLibrary1/SexGenerator/ISexGeneratorService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace SexGenerator
+{
+    [ServiceContract]
+    public interface ISexGeneratorService
+    {
+        [OperationContract]   
+        [WebInvoke(Method = "GET", UriTemplate = "/Generate/{lang}",
+            RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped)]
+        string Generate(string lang);  
+    }
+}
---
+++ WcfServiceLibrary1/SexGenerator/SexGeneratorService.svc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SexGenerator
+{
+    public class SexGeneratorService : ISexGeneratorService
+    {
+        Random r = new Random();
+        private string[][] _arr = new string[][] { new string[] {"мужской", "женский"},
+                                                   new string[] {"male", "female"},
+                                                   new string[] { "männlich", "weiblich" },
+                                                   new string[] { "mâle", "femelle" },
+                                                   new string[] { "男性", "女性" }};
+       
+        public string Generate(string value)
+        {
+            value = value.ToLower();
+          switch (value)
+            {
+                case "ru": return _arr[0][r.Next(1)];
+                case "en": return _arr[1][r.Next(1)];
+                case "de": return _arr[2][r.Next(1)];
+                case "fr": return _arr[3][r.Next(1)];
+                case "ch": return _arr[4][r.Next(1)];
+                default: return "incorrect parametr";
+
+            }
+        }
+    }
+}
