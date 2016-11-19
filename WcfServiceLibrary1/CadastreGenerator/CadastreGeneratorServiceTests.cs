0f3f0bdce66eba90e90a799a09b88f2c3efd9683
overl0rd <gmail@gmail.com>
2016-11-19 20:02:20 +0400

    edit test class

--- WcfServiceLibrary1/CadastreGenerator/CadastreGeneratorServiceTests.cs
+++ WcfServiceLibrary1/CadastreGenerator/CadastreGeneratorServiceTests.cs
@@ -7,6 +7,20 @@ namespace CadastreGenerator
     public class CadastreGeneratorServiceTests
     {
         [Test]
+        public void GenerateMethodReturnsString()
+        {
+            CadastreGeneratorService testExample = new CadastreGeneratorService();
+            var result = testExample.Generate();
+            Assert.IsTrue(result is string);
+        }
+
+        [Test]
+        public void GenerateMethodReturnsMathingResult()
+        {
+            CadastreGeneratorService testExample = new CadastreGeneratorService();
+            var result = testExample.Generate();
+            StringAssert.IsMatch(@"\d{2}:\d{2}:\d{6,7}:\d{2}", result);
+        }
 
 
     }
