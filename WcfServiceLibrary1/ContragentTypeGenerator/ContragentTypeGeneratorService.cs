212f4b2f83c2a9e5b6278f6b3fee73dc6b2fbb32
overl0rd <gmail@gmail.com>
2016-11-20 07:39:57 +0400

    add test classes

---
+++ WcfServiceLibrary1/ContragentTypeGenerator/ContragentTypeGeneratorService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ContragentTypeGenerator
+{
+    public class ContragentTypeGeneratorService
+    {
+        private string[] _typeList =
+        {
+            "Организация",
+            "Физическое лицо",
+            "Предприниматель",
+            "Филиал"
+        };
+
+        public string Generate()
+        {
+            Random rnd = new Random();
+            return _typeList[rnd.Next(_typeList.Length - 1)];
+        }
+    }
+}
---
+++ WcfServiceLibrary1/ContragentTypeGenerator/ContragentTypeGeneratorServiceTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace ContragentTypeGenerator
+{
+    [TestFixture]
+    public class ContragentTypeGeneratorServiceTests
+    {
+        [Test]
+        public void GeneratorReturnsResultNotZeroLength()
+        {
+            ContragentTypeGeneratorService testExample = new ContragentTypeGeneratorService();
+            var result = testExample.Generate();
+            Assert.IsTrue(result.Length > 0);
+        }
+    }
+}
